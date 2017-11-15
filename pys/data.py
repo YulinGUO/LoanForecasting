854a698eb4168320febae86f6529fbccf926768b
Yulin GUO <guoyulin1110@gmail.com>
2017-11-15 14:24:29 +0800

    data split added

--- pys/data.py
+++ pys/data.py
@@ -1,71 +1,67 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
-import numpy as np
+
 
 INPUT_PATH = '../input/'
 OUTPUT_PATH = '../output/'
 
 
-def load_data() :
+def load_data():
 
-	users = pd.read_csv(INPUT_PATH + 't_user.csv')
-	orders = pd.read_csv(INPUT_PATH + 't_order.csv')
-	loans = pd.read_csv(INPUT_PATH + 't_loan.csv')
-	loans_sum = pd.read_csv(INPUT_PATH + 't_loan_sum.csv')
-	clicks = pd.read_csv(INPUT_PATH + 't_click.csv')
+    users = pd.read_csv(INPUT_PATH + 't_user.csv')
+    orders = pd.read_csv(INPUT_PATH + 't_order.csv')
+    loans = pd.read_csv(INPUT_PATH + 't_loan.csv')
+    loans_sum = pd.read_csv(INPUT_PATH + 't_loan_sum.csv')
+    clicks = pd.read_csv(INPUT_PATH + 't_click.csv')
 
-	# origin price
-	loans.loan_amount = 5**loans.loan_amount
-	loans_sum.loan_sum  = 5 ** loans_sum.loan_sum
-	orders.price = 5 ** orders.price
-	orders.discount = 5 ** orders.discount 
-	users.limit = 5 ** users.limit
+    # origin price
+    loans.loan_amount = 5**loans.loan_amount
+    loans_sum.loan_sum = 5 ** loans_sum.loan_sum
+    orders.price = 5 ** orders.price
+    orders.discount = 5 ** orders.discount
+    users.limit = 5 ** users.limit
 
-	#loans	 
-	#data to month
-	loans['Date'] = pd.to_datetime(loans['loan_time'], errors='coerce')
-	loans['transaction_month'] = loans['Date'].dt.month
-	#fill nan with 0
-	loans["loan_count"] = 1
-	loans.loan_amount = loans.loan_amount.fillna(0)
+    # loans
+    loans['Date'] = pd.to_datetime(loans['loan_time'], errors='coerce')
+    loans['transaction_month'] = loans['Date'].dt.month
+    # fill nan with 0
+    loans["loan_count"] = 1
+    loans.loan_amount = loans.loan_amount.fillna(0)
+    loans_new = loans.groupby(by=["uid", "transaction_month"], as_index=False)["loan_amount", "loan_count"].sum()
+    loans_new = loans_new.pivot_table(['loan_amount', "loan_count"], ['uid'], 'transaction_month', fill_value=0)
+    loans_new.reset_index(drop=False, inplace=True)
+    loans_new.columns = ['{}_{}'.format(i[1], i[0]) for i in loans_new.columns]
+    loans_new = loans_new.rename(index=str, columns={"_uid": "uid"})
 
-	loans_new = loans.groupby(by=["uid","transaction_month"], as_index=False)["loan_amount","loan_count"].sum()
-	loans_new = loans_new.pivot_table(['loan_amount',"loan_count"], ['uid'], 'transaction_month', fill_value=0)
-	loans_new.reset_index( drop=False, inplace=True )
-	loans_new.columns = ['{}_{}'.format(i[1], i[0]) for i in loans_new.columns]
-	loans_new = loans_new.rename(index=str, columns={"_uid": "uid"})
+    # orders
+    orders['Date'] = pd.to_datetime(orders['buy_time'], errors='coerce')
+    orders['transaction_month'] = orders['Date'].dt.month
+    orders["comsume_count"] = 1
+    orders["consume_amount"] = orders["price"] * orders["qty"] - orders["discount"]
+    orders.consume_amount = orders.consume_amount.fillna(0)
+    orders_new = orders.groupby(by=["uid","transaction_month"], as_index=False)["consume_amount", "comsume_count"].sum()
+    orders_new = orders_new.pivot_table(['consume_amount', "comsume_count"], ['uid'], 'transaction_month', fill_value=0)
+    orders_new.reset_index(drop=False, inplace=True)
+    orders_new.columns = ['{}_{}'.format(i[1], i[0]) for i in orders_new.columns]
+    orders_new = orders_new.rename(index=str, columns={"_uid": "uid"})
 
+    # clicks
+    clicks['Date'] = pd.to_datetime(clicks['click_time'], errors='coerce')
+    clicks['transaction_month'] = clicks['Date'].dt.month
+    clicks["click_count"] = 1
+    clicks_new = clicks.groupby(by=["uid", "transaction_month"], as_index=False)["click_count"].sum()
+    clicks_new = clicks_new.pivot_table('click_count', ['uid'], 'transaction_month', fill_value=0)
+    clicks_new.reset_index(drop=False, inplace=True)
+    clicks_new = clicks_new.rename(index=str, columns={8: "8_click_count", 9: "9_click_count", 10 : "10_click_count", 11 : "11_click_count"})
 
-	#orders
-	orders['Date'] = pd.to_datetime(orders['buy_time'], errors='coerce')
-	orders['transaction_month'] = orders['Date'].dt.month
-	orders["comsume_count"] = 1
-
-	orders["consume_amount"] = orders["price"]*orders["qty"]-orders["discount"]
-	orders.consume_amount = orders.consume_amount.fillna(0)
-
-	orders_new = orders.groupby(by=["uid","transaction_month"], as_index=False)["consume_amount","comsume_count"].sum()
-	orders_new = orders_new.pivot_table(['consume_amount',"comsume_count"], ['uid'], 'transaction_month', fill_value=0)
-	orders_new.reset_index( drop=False, inplace=True )
-	orders_new.columns = ['{}_{}'.format(i[1], i[0]) for i in orders_new.columns]
-	orders_new = orders_new.rename(index=str, columns={"_uid": "uid"})
-
-	clicks['Date'] = pd.to_datetime(clicks['click_time'], errors='coerce')
-	clicks['transaction_month'] = clicks['Date'].dt.month
-	clicks["click_count"] = 1
-	clicks_new = clicks.groupby(by=["uid","transaction_month"], as_index=False)["click_count"].sum()
-	clicks_new = clicks_new.pivot_table('click_count', ['uid'], 'transaction_month', fill_value=0)
-	clicks_new.reset_index( drop=False, inplace=True )
-	clicks_new = clicks_new.rename(index=str, columns={8: "click_count_8", 9: "click_count_9", 10 : "click_count_10", 11 : "click_count_11"})
-
-	return users, orders_new, loans_new, loans_sum, clicks_new
+    return users, orders_new, loans_new, loans_sum, clicks_new
 
 if __name__ == "__main__":
-	print('begin to load data')
-	users, orders, loans, loan_sum, clicks = load_data()
-	print(' load data finished')
-
-	users_orders = users.merge(orders ,how='left', on="uid")
-	users_orders_loans = users_orders.merge(loans ,how='left', on="uid")
-	user_info = users_orders_loans.merge(clicks ,how='left', on="uid")
-	print('to csv ........')
-	user_info.to_csv(OUTPUT_PATH +"user_info.csv", index=False)
+    print('begin to load data')
+    users, orders, loans, loan_sum, clicks = load_data()
+    print(' load data finished')
+    users_orders = users.merge(orders ,how='left', on="uid")
+    users_orders_loans = users_orders.merge(loans ,how='left', on="uid")
+    user_info = users_orders_loans.merge(clicks ,how='left', on="uid")
+    print('to csv ........')
+    user_info.to_csv(OUTPUT_PATH +"user_info.csv", index=False)
---
+++ pys/datasplit.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import re
+
+import pandas as pd
+
+
+INPUT_PATH = '../input/'
+OUTPUT_PATH = '../output/'
+CC = '{}_comsume_count'
+CM = '{}_consume_amount'
+LM = '{}_loan_amount'
+LC = '{}_loan_count'
+CKC = '{}_click_count'
+CUM = '_cum'
+basic = ['uid', 'age', 'sex', 'active_date', 'limit']
+TARGET = 'target'
+
+
+def load_data():
+    user_info = pd.read_csv(OUTPUT_PATH + 'user_info.csv')
+    for c in user_info.columns:
+        user_info[c] = user_info[c].fillna(0)
+    # cummulated data
+    for month in [8, 9, 10, 11, 12]:
+        user_info = cummulate_data_by_month(user_info, month)
+
+    df8 = get_df_by_month(user_info, '8')
+    df8 = add_target_by_month(df8, 8, user_info)
+    df8 = df8.rename(columns=remove_month_rename)
+    df9 = get_df_by_month(user_info, '9')
+    df9 = add_target_by_month(df9, 9, user_info)
+    df9 = df9.rename(columns=remove_month_rename)
+    df10 = get_df_by_month(user_info, '10')
+    df10 = add_target_by_month(df10, 10, user_info)
+    df10 = df10.rename(columns=remove_month_rename)
+    df11 = get_df_by_month(user_info, '11')
+    df11 = df11.rename(columns=remove_month_rename)
+
+    frames = [df8, df9, df10]
+    result = pd.concat(frames)
+    return result, df11
+
+
+def cummulate_data_by_month(df, month):
+    cc_bef = CC.format(month - 1) + CUM
+    cm_bef = CM.format(month - 1) + CUM
+    lm_bef = LM.format(month - 1) + CUM
+    lc_bef = LC.format(month - 1) + CUM
+    ckc_bef = CKC.format(month - 1) + CUM
+
+    cc_cur = CC.format(month - 1)
+    cm_cur = CM.format(month - 1)
+    lm_cur = LM.format(month - 1)
+    lc_cur = LC.format(month - 1)
+    ckc_cur = CKC.format(month - 1)
+
+    cc_cum = CC.format(month) + CUM
+    cm_cum = CM.format(month) + CUM
+    lm_cum = LM.format(month) + CUM
+    lc_cum = LC.format(month) + CUM
+    ckc_cum = CKC.format(month) + CUM
+
+    if month == 8:
+        df[cc_cum] = 0
+        df[cm_cum] = 0
+        df[lm_cum] = 0
+        df[lc_cum] = 0
+        df[ckc_cum] = 0
+    else:
+        df[cc_cum] = df[cc_cur] + df[cc_bef]
+        df[cm_cum] = df[cm_cur] + df[cm_bef]
+        df[lm_cum] = df[lm_cur] + df[lm_bef]
+        df[lc_cum] = df[lc_cur] + df[lc_bef]
+        df[ckc_cum] = df[ckc_cur] + df[ckc_bef]
+
+    return df
+
+
+def add_target_by_month(df, month, user_info):
+    lm_next = LM.format(month + 1)
+    df[TARGET] = user_info[lm_next]
+    return df
+
+
+def get_column_by_month(df, month):
+    return list(df.columns[df.columns.str.startswith(month)])
+
+
+def get_df_by_month(df, month):
+    cols = get_column_by_month(df, month)
+    basic = ['uid', 'age', 'sex', 'active_date', 'limit']
+    all_cols = basic +cols 
+    return df[all_cols]
+
+
+def remove_month_rename(col_name):
+    if re.match(r"(\d{1,2}_)(.+)", col_name):
+        m = re.search(r"(\d{1,2}_)(.+)", col_name)
+        return m.group(2)
+    else:
+        return col_name
+
+
+if __name__ == "__main__":
+    print('begin to load data')
+    train, submit = load_data()
+    print('to csv ........')
+    train.to_csv(INPUT_PATH +"trainv1.csv", index=False)
+    submit.to_csv(INPUT_PATH +"submitv1.csv", index=False)
+
